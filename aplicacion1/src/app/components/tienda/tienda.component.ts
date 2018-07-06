d2f7e432ae7442c883cb43b57df7013d00e146f2
joselinevg4@gmail.com <joselinevg4@gmail.com>
2018-07-06 17:35:35 -0600

    cambio angular

---
+++ aplicacion1/src/app/components/tienda/tienda.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tienda',
+  templateUrl : '/tienda.component.html',
+  styleUrls: ['/tienda.component.css']
+})
+export class TiendaComponent {
+  public titulo;
+  constructor(){
+      this.titulo="esta es mi tienda";
+  }
+}
